7f578a68b17d44c7941c37315db29272fed41306
huangtao <huangtao2999>
2018-01-17 16:09:54 +0800

    优化接口

--- src/main/java/com/dsw/guankong/sys/BizExceptionHandler.java
+++ src/main/java/com/dsw/guankong/sys/BizExceptionHandler.java
@@ -2,6 +2,7 @@ package com.dsw.guankong.sys;
 
 import com.dsw.guankong.util.ActionResult;
 import com.dsw.guankong.util.BizException;
+
 import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+
 import java.text.MessageFormat;
 
 /**
@@ -18,19 +20,21 @@ import java.text.MessageFormat;
 @ControllerAdvice
 public class BizExceptionHandler {
     protected static final Logger logger = Logger.getLogger(BizExceptionHandler.class);
+
     @ExceptionHandler(value = BizException.class)
     @ResponseBody
     public Object bizExceptionHandler(HttpServletRequest req, Exception e) throws Exception {
-        logger.error(MessageFormat.format("---BizException Handler---Host {0} invokes url {1}", req.getRemoteHost(), req.getRequestURL()));
+        logger.error(MessageFormat.format("---BizException Handler---Host {0} invokes url {1} params{2}", req.getRemoteHost(), req.getRequestURL(), req.getParameterMap()));
         ActionResult actionResult = new ActionResult();
         actionResult.setSuccess(false);
         actionResult.setErrorMsg(e.getMessage());
         return actionResult;
     }
+
     @ExceptionHandler(value = Exception.class)
     @ResponseBody
     public Object exceptionHandler(HttpServletRequest req, Exception e) throws Exception {
-        logger.error(MessageFormat.format("---Exception Handler---Host {0} invokes url {1} ERROR: {2}", req.getRemoteHost(), req.getRequestURL(), e.getMessage()),e);
+        logger.error(MessageFormat.format("---Exception Handler---Host {0} invokes url {1} ERROR: {2} params{3}", req.getRemoteHost(), req.getRequestURL(), e.getMessage(), req.getParameterMap()), e);
         ActionResult actionResult = new ActionResult();
         actionResult.setSuccess(false);
         actionResult.setErrorCode("00000");
